5f12095cc8bf454d3a7cdfd43dc1d152500edb71
nguyentuanlamtn <nguyentuanlamtn@gmail.com>
2022-07-04 12:44:30 +0700

    hi

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,103 @@
+import React from 'react'
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+
+import logo from "../../assets/images/logo.png";
+
+import "../../styles/footer.css";
+
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="3" md="4" sm="6">
+            <div className=" footer__logo text-start">
+              <img src={logo} alt="logo" />
+              <h5>Kingdom Food</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Nesciunt pariatur accusamus
+              </p>
+            </div>
+
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Delivery Time</h5>
+            <ListGroup className="deliver__time-list">
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Sunday - Friday</span>
+                <p>10:00am - 11:00pm</p>
+              </ListGroupItem>
+
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Saturday - Sunday</span>
+                <p>10:00am - 9:00pm</p>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Contact</h5>
+            <ListGroup className="deliver__time-list">
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <p>Location: Viet Yen, Bich Dong, Bac Giang</p>
+              </ListGroupItem>
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Phone: 0987654321</span>
+              </ListGroupItem>
+
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Email: kingdomfood@gmail.com</span>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Newsletter</h5>
+            <p>Subscribe our newsletter</p>
+            <div className="newsletter">
+              <input type="email" placeholder="Enter your email" />
+              <span>
+                <i className="ri-send-plane-line"></i>
+              </span>
+            </div>
+          </Col>
+        </Row>
+
+        <Row className="mt-5">
+          <Col lg="6" md="6">
+            <p className="copyright__text">
+              Copyright - 2022. All Rights
+              Reserved.
+            </p>
+          </Col>
+          <Col lg="6" md="6">
+            <div className="social__links d-flex align-items-center gap-4 justify-content-end">
+              <p className="m-0">Follow: </p>
+
+              <span>
+                  <i className="ri-facebook-line"></i>
+              </span>
+
+              <span>
+                  <i className="ri-github-line"></i>
+              </span>
+
+              <span>
+                  <i className="ri-youtube-line"></i>
+              </span>
+
+              <span>
+                  <i className="ri-linkedin-line"></i>
+              </span>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
